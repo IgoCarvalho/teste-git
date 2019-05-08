9e088fb44a18c6d6ad2092d2bb436886625aa546
Igo Carvalho <igocarvalho00@gmail.com>
2019-05-08 15:28:33 -0300

    novo footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './components/header';
+import Footer from './components/footer';
 
 // import Users from './pages/users'
 
@@ -18,6 +19,7 @@ const App = () => (
         </div>
       </div>
     </div>
+    <Footer />
 
   </div>
 );
---
+++ src/components/footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Footer = () => (
+    <div>EU SOU UM Footer</div>
+);
+
+export default Footer;
